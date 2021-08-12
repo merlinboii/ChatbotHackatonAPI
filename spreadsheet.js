1dde27fb94be53acc008d5af78ee455d3719d16e
Parichaya <parichaya35@gmail.com>
2021-08-12 18:57:01 +0700

    update server

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,22 +1,16 @@
-const GoogleSpreadsheet = require('google-spreadsheet')
-// const { promisify } = require('util')
 var express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
 
+require('dotenv').config()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
 var route = require('./routes/routes')
 route(app)
 
-//================= PORT ================//
  app.listen(process.env.PORT || 80 , function () {
     console.log('App listening on port 3000!');
   })  
-
-/*   app.listen(3000 , function () {
-    console.log('App listening on port 3000!');
-  }) */  
 
  
